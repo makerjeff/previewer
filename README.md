d4e65ef3765b8cd29a80d1c82b5a1a4bbd43571b
Jeff Wu <makerjeffwu@gmail.com>
2016-04-01 17:32:28 -0700

    working jade basic template

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ Helena's previewer
 - [Multer: File upload](https://www.npmjs.com/package/multer)
 - [bcrypt](https://www.npmjs.com/package/bcryptjs)
 
+
+### Based on:
+- [Simple JADE/Express tutorial](http://www.clock.co.uk/blog/a-simple-website-in-nodejs-with-express-jade-and-stylus)
+- [Node Express Mongo Jade](http://cwbuecheler.com/web/tutorials/2013/node-express-mongo/)
 ### Notes/Major Discoveries
 
 
--- server.js
+++ server.js
@@ -5,16 +5,14 @@
 /* ============ MODULES ============ */
 var express = require('express');
 var multer = require('multer');
+var jade = require('jade');
 var colors = require('colors');
 var fileType = require('file-type');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var mongoose = require('mongoose');
 
 // === custom modules ===
 var previewer = require('./previewer-module');
-var User = require('./models/user');
-
 /* ============ GLOBALS ============ */
 var done = false;
 
@@ -33,13 +31,12 @@ var storage = multer.diskStorage({
 var upload = multer({storage: storage}).single('uploadFile');
 var multi = multer({storage: storage}).array('uploadFiles');
 
-// == mongoDB ==
-mongoose.connect('mongodb://localhost/mongerd');
-
 /* ============ MIDDLEWARE ============ */
-
+//use jade
+app.set('view engine', 'jade');
 //server static files
 app.use(express.static(__dirname + "/public/"));
+
 
 //enable bodyParser
 app.use(bodyParser.json());
@@ -67,41 +64,24 @@ app.get('/', function(request, response){
     response.sendFile(__dirname + '/public/index.html');
 });
 
-
-/* Standard form POST route */
-app.post('/api/signup', function(request, response){
-
-    var bString = '';
-
-    //request.body.<variable> comes across unresolved according to webstorm, but works.
-    bString = request.body.firstname + ' ' + request.body.lastname + ' pin: ' + request.body.pin;
-
-    console.dir(bString);
-
-    //createRandomFolder();
-    previewer.logToFile(bString);
-
-    response.type('text/html');
-    response.end(bString);
+//JADE helloworld
+app.get('/helloworld', function(request, response){
+    //(<string: name of jade file>, {<object: params>});
+    response.render('helloworld', {title: 'Hello, World!'});
 });
 
-
-/* AJAX single file upload route */
-app.post('/api/upload', function(request, response){
-
-    // multer singleFile upload
-    upload(request, response, function(error){
-
-        if(error) {
-            return response.end("error uploading file!");
-        }
-
-        response.type('text/html');
-        response.end('File is uploaded. ' + '<a href="/upload.html">Upload more!</a>');
-        //console.log(request.body);
-        console.log('file uploaded.');
-    });
+app.get('/helloworld/:string', function(request,response){
+    response.render('helloworld', {title: request.params.string});
 });
+
+app.get('/form/', function(request,response){
+    response.render('form', {title: 'upload'});
+});
+
+app.get('/index', function(request, response){
+    response.render('index', {title: 'Index page!'});
+});
+
 
 /* AJAX multiple file upload route */
 app.post('/api/multi', function(request, response){
@@ -109,12 +89,11 @@ app.post('/api/multi', function(request, response){
     multi(request, response, function(error){
         if(error){
             return response.end('error uploading files!');
+        } else {
+            response.type('text/html');
+            response.end('Files have been uploaded. ' + '<a href="/multi-upload.html">Upload again! </a>');
+            console.log(request.files);
         }
-
-        response.type('text/html');
-        response.end('Files have been uploaded. ' + '<a href="/multi-upload.html">Upload again! </a>');
-
-        console.log(request.files);
     });
 });
 
@@ -122,60 +101,14 @@ app.post('/api/multi', function(request, response){
 /* AJAX multi-file upload with auto unzip and folder creation */
 // TODO dev
 app.post('/api/unzip', function(request, response){
-
     multi(request, response, function(error){
         if(error){
             return response.end('error uploading files!');
         }
-
         response.type('text/html');
         response.end('Files have been uploaded and unzipped to root/public/unzipped');
     });
 });
-
-// check to see if db is responding
-//default route
-app.get('/api/databasedebug/', function(request, response){
-
-    var chris = new User({
-        name: 'chris',
-        username: 'sevilayha',
-        password: 'password'
-    });
-
-    //dudify
-    chris.dudify(function(error, name){
-        if(error){
-            return console.log(Error(error));
-        } else {
-            console.log('adding ' + name);
-        }
-    });
-
-    chris.save(function(error){
-        if(error){
-            console.log(Error(error));
-        } else {
-            console.log('User saved');
-        }
-    });
-
-    response.type('text/html');
-    response.end('attempting to add a user');
-});
-
-//custom query route
-//TODO custom get-route for querying DB
-// get for queries, post for updates
-
-app.get('/api/databasedebug/:user', function(request, response){
-    console.log(request.params.user);
-
-    response.type('text/html');
-    response.end('You are trying to find ' + request.params.user);
-});
-
-
 
 
 // basic 404 catch-all middleware
@@ -187,10 +120,5 @@ app.get('*', function(request, response){
 app.listen(3000, function(){
     console.log('Working on Port 3000'.blue);
 });
-
-//TODO auto-unzip
-
-//TODO create folderArray of existing folders
-
 
 /* ============ HELPER FUNCTIONS ============ */
